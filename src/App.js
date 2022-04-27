c82732b447eaea129702e4f8945712ba15535081
Gino Oliveira <lucianaegino02@hotmail.com>
2022-04-26 23:11:50 -0300

    Costs

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Container from './components/layout/Container';
+import Navbar from './components/layout/Navbar';
+import Company from './components/pages/Company';
+import Contact from './components/pages/Contact';
+import Home from './components/pages/Home';
+import NewProjet from './components/pages/NewProjet';
+import Footer from './components/layout/Footer'
+import Projects from './components/pages/projects';
+
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   
+    <Router>
+        <Navbar/>
+        
+        <Container custonClass= "min-height"> 
+          <Routes>
+            <Route exact path="/" element={<Home />} ></Route>
+            <Route exact path="/contact" element={<Contact />}></Route>
+            <Route exact path="/projects" element={<Projects />}></Route>
+            <Route exact path="/company" element={<Company />}></Route>
+            <Route exact path="/newprojet" element={<NewProjet />}></Route>
+          </Routes>
+        </Container>
+        <Footer/>
+     </Router>
+     
   );
 }
 
---
+++ src/components/pages/NewProjet.js
@@ -0,0 +1,13 @@
+import ProjectForm from '../project/ProjectForm'
+import styles from './NewProjet.module.css'
+
+function NewProjet(){
+    return(
+        <div className={styles.newproject_container} >
+            <h1>Criar Projeto</h1>
+            <p>Crie seu projeto para depois adicionar os serviços</p>
+            <ProjectForm/>
+        </div>
+    )
+}
+export default NewProjet
